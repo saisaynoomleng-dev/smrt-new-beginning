9f8e556e93812d9b161db10d12b899d111fe47fa
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2026-02-09 15:24:26 -0500

    finish seedData

--- README.md
+++ README.md
@@ -12,11 +12,14 @@
 - postgresql(neon DB)
 - resend (email)
 - drizzle(orm)
+- redis
+- resend
+- vercel blob
 
 ---
 
 ## Project Timeline
 
-| Date     | Description                          |
-| -------- | ------------------------------------ |
-| 2/9/2026 | - Project Research<br> - Development |
+| Date     | Description                                                |
+| -------- | ---------------------------------------------------------- |
+| 2/9/2026 | - Project Research<br> - setup database <br> - Development |
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,47 +3,47 @@ import localFont from 'next/font/local';
 export const roboto = localFont({
   src: [
     {
-      path: '../src/app/fonts/Roboto-Thin.ttf',
+      path: '../app/fonts/Roboto-Thin.ttf',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-ExtraLight.ttf',
+      path: '../app/fonts/Roboto-ExtraLight.ttf',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-Light.ttf',
+      path: '../app/fonts/Roboto-Light.ttf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-Regular.ttf',
+      path: '../app/fonts/Roboto-Regular.ttf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-Medium.ttf',
+      path: '../app/fonts/Roboto-Medium.ttf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-SemiBold.ttf',
+      path: '../app/fonts/Roboto-SemiBold.ttf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-Bold.ttf',
+      path: '../app/fonts/Roboto-Bold.ttf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-ExtraBold.ttf',
+      path: '../app/fonts/Roboto-ExtraBold.ttf',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../src/app/fonts/Roboto-Black.ttf',
+      path: '../app/fonts/Roboto-Black.ttf',
       weight: '900',
       style: 'normal',
     },
